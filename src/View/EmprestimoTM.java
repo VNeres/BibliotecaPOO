deac9bca392a43de4ca9a15499ac8282b53fb73e
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2019-05-27 20:30:52 -0300

    Criando EmprestimoTM

--- src/View/EmprestimoTM.java
+++ src/View/EmprestimoTM.java
@@ -1,14 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
-/**
- *
- * @author vande
- */
-public class EmprestimoTM {
-    
+import Model.Emprestimo;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.AbstractTableModel;
+
+public class EmprestimoTM extends AbstractTableModel {
+
+    private List<Emprestimo> linhas;
+    private String[] colunas = new String[]{"Código", "Livro Emprestado", "Emprestado Para", "Data Empréstimo", "Data Devolução"};
+
+    public EmprestimoTM() {
+        linhas = new ArrayList<Emprestimo>();
+    }
+
+    public EmprestimoTM(ArrayList<Emprestimo> emprestimos) {
+        linhas = new ArrayList<Emprestimo>(emprestimos);
+    }
+
+    @Override
+    public int getRowCount() {
+        return linhas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int linha, int coluna) {
+
+        switch (coluna) {
+            case 0:
+                return linhas.get(linha).getID();
+            case 1:
+                return linhas.get(linha).getLivroEmprestado();
+            case 2:
+                return linhas.get(linha).getEmprestadoPara();
+            case 3:
+                return linhas.get(linha).getDataEmprestimo();
+            case 4:
+                return linhas.get(linha).getDataDevolucao();
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object valor, int linha, int coluna) {
+
+        switch (coluna) {
+            case 0:
+                linhas.get(linha).setID(Integer.parseInt((String) valor));
+                break;
+            case 1:
+                linhas.get(linha).setLivroEmprestado((String) valor);
+                break;
+            case 2:
+                linhas.get(linha).setEmprestadoPara((String) valor);
+                break;
+            case 3:
+                linhas.get(linha).setDataEmprestimo(new Date());
+                break;
+            case 4:
+                try {
+                    linhas.get(linha).setDataDevolucao(new SimpleDateFormat("dd/MM/yyyy").parse((String) valor));
+                } catch (ParseException ex) {
+                    Logger.getLogger(EmprestimoTM.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
+        }
+    }
+
 }
