7df36af0b068f8faa982c9a723bd4d0a1b755f51
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2019-05-27 20:40:08 -0300

    Criando LivroTM

--- src/View/LivroTM.java
+++ src/View/LivroTM.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
-/**
- *
- * @author vande
- */
-public class LivroTM {
+import Model.Livro;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class LivroTM extends AbstractTableModel {
+
+    private List<Livro> linhas;
+    private String[] colunas = new String[]{"Código", "Nome", "Autor", "Ano", "Disponível?"};
+    
+    @Override
+    public int getRowCount() {
+        return linhas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int linha, int coluna) {
+        
+        switch (coluna) {
+            case 0:
+                return linhas.get(linha).getId();
+            case 1:
+                return linhas.get(linha).getNome();
+            case 2:
+                return linhas.get(linha).getAutor();
+            case 3:
+                return linhas.get(linha).getAno();
+            case 4:
+                return linhas.get(linha).isIsDisponivel();
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object valor, int linha, int coluna) {
+        
+        
+        switch (coluna) {
+            case 0:
+                linhas.get(linha).setId(Integer.parseInt((String) valor));
+                break;
+            case 1:
+                linhas.get(linha).setNome((String) valor);
+                break;
+            case 2:
+                linhas.get(linha).setAutor((String) valor);
+                break;
+            case 3:
+                linhas.get(linha).setAno(Integer.parseInt((String) valor));
+                break;
+            case 4:
+                linhas.get(linha).setIsDisponivel(Boolean.parseBoolean((String) valor));
+                break;
+        }
+    }
+    
     
 }
